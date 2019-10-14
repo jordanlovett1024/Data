7f8a6eef3eaa4e2de24594e448a1896363046f0f
jordanlovett1024 <48740970+jordanlovett1024@users.noreply.github.com>
2019-10-14 10:34:52 -0500

    Create missing_columns.py

---
+++ missing_columns.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import sys
+import os
+
+#get list of every file in mapping files 
+path = '/../../home/labs/holscher_lab/alex/AGP_2019/mapping_files/'
+os.system("ls /../../home/labs/holscher_lab/alex/AGP_2019/mapping_files/ > files.txt")
+
+with open('files.txt') as f:
+    filenames = f.readlines()
+filenames = [x.strip() for x in content]
+
+def count_gaps(path,tsv,gaps,entries):
+    df = pd.read_csv(path+tsv,sep='\t')
+    for i in df.columns: df[i] = df[i].astype(str)
+    
+    for col_index,col_value in enumerate(df.columns):
+        entries[col_index] = entries[col_index] + len(df[col_value])
+        gaps[col_index] = gaps[col_index] + len([i for i in df[col_value] if i.__contains__('Not provided')
+                               or i.__contains__('nan')])  
+gaps = np.zeros((len(df.columns),))
+entries = np.zeros(gaps.shape)
+
+for i in range(len(filenames)):
+    count_gaps(path,filenames[i],gaps,entries)
+
+results = pd.DataFrame({'Name':df.columns,'Missing':gaps,'Occurences':entries, '% Missing':gaps/entries *100})
+results.to_csv('results.tsv',sep='\t')
